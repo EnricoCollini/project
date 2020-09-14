3a9c3c0608ba3c55fd99ab95033c2addd5ecbd01
enrico <encollini@gmail.com>
2020-09-14 14:09:06 +0200

    first commit

---
+++ src/main/java/model/AreaNaturale.java
@@ -0,0 +1,44 @@
+package model;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+
+public class AreaNaturale{
+
+	private Long id;
+	private String nome;
+	
+	
+	
+	@Id
+	@GeneratedValue( strategy=GenerationType.AUTO )
+	@Column(name="id", nullable = false)
+	@NotNull
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+}
