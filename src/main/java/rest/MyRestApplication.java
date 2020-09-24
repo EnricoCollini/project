835bb210743677eab4b9c6681806a621b70712a5
enrico <encollini@gmail.com>
2020-09-24 13:36:51 +0200

    buggettino fixed

---
+++ src/main/java/rest/MyRestApplication.java
@@ -0,0 +1,13 @@
+package rest;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("rest")
+public class MyRestApplication extends Application{
+	//No methods required
+}
+
+
+
+
+
